b851bcdaa8fcce0e4efad0129bd5c8e691019a31
Mateus Felipe <mateus@pop-os.localdomain>
2022-07-05 09:56:32 -0300

    first commit :)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Base } from './components/Base'
+
+function App() {
+  return (
+    <div className="bg-gradient-to-b from-violet-500 to-pink-400 w-screen h-screen">
+      <div className="container bg-gray-300 max-h-fit max-w-md w-3/4 border rounded-xl mx-auto flex justify-center ">
+        <ul>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Base.tsx
@@ -0,0 +1,8 @@
+export function Base() {
+  return (
+    <div>
+      <img src="home-page/src/assets/profile_pic.png" alt="profile_pic" />
+      <p>Mateus</p>
+    </div>
+  )
+}
